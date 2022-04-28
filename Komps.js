343dd031377b9c73f9a40ba8b2ffbfef313f49ae
elmirrzayev <elmirtanki@gmail.com>
2022-04-28 10:28:15 +0400

    Adini ne qoyum bilmirem

---
+++ Komps.js
@@ -0,0 +1,90 @@
+$('#button').click(function(){
+var a = $('#a').val();
+var b = $('#b').val();
+var c = $('#c').val();
+var d = $('#d').val();
+var e = $('#e').val();
+var f = $('#f').val();
+var g = $('#g').val();
+var i = $('#i').val();
+var j = $('#j').val();
+var k = $('#k').val();
+var q = $('#q').val();
+if(a == ""){
+    $('#a').classList.remove("is-valid");
+    $('#a').classList.add("is-invalid");
+}else{
+    $('#a').classList.remove("is-invalid");
+    $('#a').classList.add("is-valid");
+}
+if(b == ""){
+    $('#b').classList.remove("is-valid");
+    $('#b').classList.add("is-invalid");
+}else{
+    $('#b').classList.remove("is-invalid");
+    $('#b').classList.add("is-valid");
+}
+if(c == ""){
+    $('#c').classList.remove("is-valid");
+    $('#c').classList.add("is-invalid");
+}else{
+    $('#c').classList.remove("is-invalid");
+    $('#c').classList.add("is-valid");
+}
+if(d == ""){
+    $('#d').classList.remove("is-valid");
+    $('#d').classList.add("is-invalid");
+}else{
+    $('#d').classList.remove("is-invalid");
+    $('#d').classList.add("is-valid");
+}
+if(e == ""){
+    $('#e').classList.remove("is-valid");
+    $('#e').classList.add("is-invalid");
+}else{
+    $('#e').classList.remove("is-invalid");
+    $('#e').classList.add("is-valid");
+}
+if(f == ""){
+    $('#f').classList.remove("is-valid");
+    $('#f').classList.add("is-invalid");
+}else{
+    $('#f').classList.remove("is-invalid");
+    $('#f').classList.add("is-valid");
+}
+if(g == ""){
+    $('#g').classList.remove("is-valid");
+    $('#g').classList.add("is-invalid");
+}else{
+    $('#g').classList.remove("is-invalid");
+    $('#g').classList.add("is-valid");
+}
+if(i == ""){
+    $('#i').classList.remove("is-valid");
+    $('#i').classList.add("is-invalid");
+}else{
+    $('#i').classList.remove("is-invalid");
+    $('#i').classList.add("is-valid");
+}
+if(j == ""){
+    $('#j').classList.remove("is-valid");
+    $('#j').classList.add("is-invalid");
+}else{
+    $('#j').classList.remove("is-invalid");
+    $('#j').classList.add("is-valid");
+}
+if(k == ""){
+    $('#k').classList.remove("is-valid");
+    $('#k').classList.add("is-invalid");
+}else{
+    $('#k').classList.remove("is-invalid");
+    $('#k').classList.add("is-valid");
+}
+if(q == ""){
+    $('#q').classList.remove("is-valid");
+    $('#q').classList.add("is-invalid");
+}else{
+    $('#q').classList.remove("is-invalid");
+    $('#q').classList.add("is-valid");
+}
+});
